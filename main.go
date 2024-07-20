312f2ed1069ce4a1d9d3a7ab2cac55dea0334e90
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-07-20 20:08:15 +0300

    updates

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ const (
 	done
 )
 
+/* MODEL MANAGEMENT */
 var models []tea.Model
 
 const (
@@ -28,9 +29,11 @@ const (
 	form
 )
 
+/* STYLING */
 var (
 	columnStyle = lipgloss.NewStyle().
-			Padding(1, 2)
+			Padding(1, 2).
+			Border(lipgloss.HiddenBorder())
 	focusedStyle = lipgloss.NewStyle().
 			Padding(1, 2).
 			Border(lipgloss.RoundedBorder()).
@@ -39,6 +42,8 @@ var (
 			Foreground(lipgloss.Color("241"))
 )
 
+/* CUSTOM ITEM */
+
 type Task struct {
 	status      status
 	title       string
@@ -46,7 +51,7 @@ type Task struct {
 }
 
 func NewTask(status status, title, description string) Task {
-	return Task{title: title, description: description, status: status}
+	return Task{status: status, title: title, description: description}
 }
 
 func (t *Task) Next() {
@@ -57,6 +62,7 @@ func (t *Task) Next() {
 	}
 }
 
+// implement the list.Item interface
 func (t Task) FilterValue() string {
 	return t.title
 }
@@ -69,34 +75,66 @@ func (t Task) Description() string {
 	return t.description
 }
 
+/* MAIN MODEL */
+
 type Model struct {
-	focused status
-	lists   []list.Model
-	err     error
-	loaded  bool
-	quiting bool
+	loaded   bool
+	focused  status
+	lists    []list.Model
+	err      error
+	quitting bool
+}
+
+func New() *Model {
+	return &Model{}
+}
+
+func (m *Model) MoveToNext() tea.Msg {
+	selectedItem := m.lists[m.focused].SelectedItem()
+	selectedTask := selectedItem.(Task)
+	m.lists[selectedTask.status].RemoveItem(m.lists[m.focused].Index())
+	selectedTask.Next()
+	m.lists[selectedTask.status].InsertItem(len(m.lists[selectedTask.status].Items())-1, list.Item(selectedTask))
+	return nil
+}
+
+func (m *Model) Next() {
+	if m.focused == done {
+		m.focused = todo
+	} else {
+		m.focused++
+	}
+}
+
+func (m *Model) Prev() {
+	if m.focused == todo {
+		m.focused = done
+	} else {
+		m.focused--
+	}
 }
 
 func (m *Model) initLists(width, height int) {
-	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divisor, height-divisor)
+	defaultList := list.New([]list.Item{}, list.NewDefaultDelegate(), width/divisor, height/2)
 	defaultList.SetShowHelp(false)
 	m.lists = []list.Model{defaultList, defaultList, defaultList}
 
+	// Init To Do
 	m.lists[todo].Title = "To Do"
 	m.lists[todo].SetItems([]list.Item{
-		Task{status: todo, title: "Buy Milk", description: "Get some milk from the store"},
-		Task{status: todo, title: "Eat Sushi", description: "Get some sushi from the store"},
-		Task{status: todo, title: "Fold Laundry", description: "God Please"},
+		Task{status: todo, title: "buy milk", description: "strawberry milk"},
+		Task{status: todo, title: "eat sushi", description: "negitoro roll, miso soup, rice"},
+		Task{status: todo, title: "fold laundry", description: "or wear wrinkly t-shirts"},
 	})
-
+	// Init in progress
 	m.lists[inProgress].Title = "In Progress"
 	m.lists[inProgress].SetItems([]list.Item{
-		Task{status: inProgress, title: "Stay Cool", description: "As always"},
+		Task{status: todo, title: "write code", description: "don't worry, it's Go"},
 	})
-
+	// Init done
 	m.lists[done].Title = "Done"
 	m.lists[done].SetItems([]list.Item{
-		Task{status: done, title: "Be a Good Developer", description: "It is easy"},
+		Task{status: todo, title: "stay cool", description: "as a cucumber"},
 	})
 }
 
@@ -118,86 +156,56 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
-			m.quiting = true
+			m.quitting = true
 			return m, tea.Quit
 		case "left", "h":
-			m.Previous()
+			m.Prev()
 		case "right", "l":
 			m.Next()
 		case "enter":
 			return m, m.MoveToNext
 		case "n":
-			models[model] = m
+			models[model] = m // save the state of the current model
 			models[form] = NewForm(m.focused)
 			return models[form].Update(nil)
 		}
 	case Task:
 		task := msg
-		return m, m.lists[task.status].InsertItem(
-			len(m.lists[task.status].Items()), task)
+		return m, m.lists[task.status].InsertItem(len(m.lists[task.status].Items()), task)
 	}
 	var cmd tea.Cmd
 	m.lists[m.focused], cmd = m.lists[m.focused].Update(msg)
 	return m, cmd
 }
 
-func New() *Model {
-	return &Model{}
-}
-
-func (m *Model) MoveToNext() tea.Msg {
-	selectedItem := m.lists[m.focused].SelectedItem()
-	selectedTask := selectedItem.(Task)
-	m.lists[selectedTask.status].RemoveItem(m.lists[m.focused].Index())
-	selectedTask.Next()
-	m.lists[selectedTask.status].InsertItem(len(m.lists[selectedTask.status].Items())-1, selectedTask)
-	return nil
-}
-
-func (m *Model) Next() {
-	if m.focused == done {
-		m.focused = todo
-	} else {
-		m.focused++
-	}
-}
-
-func (m *Model) Previous() {
-	if m.focused == todo {
-		m.focused = done
-	} else {
-		m.focused--
-	}
-}
-
 func (m Model) View() string {
-	if m.quiting {
+	if m.quitting {
 		return ""
 	}
 	if m.loaded {
 		todoView := m.lists[todo].View()
-		inProgressView := m.lists[inProgress].View()
+		inProgView := m.lists[inProgress].View()
 		doneView := m.lists[done].View()
 		switch m.focused {
 		case inProgress:
 			return lipgloss.JoinHorizontal(
 				lipgloss.Left,
 				columnStyle.Render(todoView),
-				focusedStyle.Render(inProgressView),
+				focusedStyle.Render(inProgView),
 				columnStyle.Render(doneView),
 			)
 		case done:
 			return lipgloss.JoinHorizontal(
 				lipgloss.Left,
 				columnStyle.Render(todoView),
-				columnStyle.Render(inProgressView),
+				columnStyle.Render(inProgView),
 				focusedStyle.Render(doneView),
 			)
 		default:
 			return lipgloss.JoinHorizontal(
 				lipgloss.Left,
 				focusedStyle.Render(todoView),
-				columnStyle.Render(inProgressView),
+				columnStyle.Render(inProgView),
 				columnStyle.Render(doneView),
 			)
 		}
@@ -206,50 +214,11 @@ func (m Model) View() string {
 	}
 }
 
+/* FORM MODEL */
 type Form struct {
 	focused     status
 	title       textinput.Model
 	description textarea.Model
-}
-
-func (f Form) Init() tea.Cmd {
-	return nil
-}
-
-func (f Form) CreateTask() tea.Msg {
-	task := NewTask(f.focused, f.title.Value(), f.description.Value())
-	return task
-}
-
-func (f Form) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return f, tea.Quit
-		case "enter":
-			if f.title.Focused() {
-				f.title.Blur()
-				f.description.Focus()
-				return f, textarea.Blink
-			} else {
-				models[form] = f
-				return models[model], f.CreateTask
-			}
-		}
-	}
-	if f.title.Focused() {
-		f.title, cmd = f.title.Update(msg)
-		return f, cmd
-	} else {
-		f.description, cmd = f.description.Update(msg)
-		return f, cmd
-	}
-}
-
-func (f Form) View() string {
-	return lipgloss.JoinVertical(lipgloss.Left, f.title.View(), f.description.View())
 }
 
 func NewForm(focused status) *Form {
@@ -257,16 +226,55 @@ func NewForm(focused status) *Form {
 	form.title = textinput.New()
 	form.title.Focus()
 	form.description = textarea.New()
-	fmt.Printf("form: %v\n", form)
 	return form
 }
 
+func (m Form) CreateTask() tea.Msg {
+	task := NewTask(m.focused, m.title.Value(), m.description.Value())
+	return task
+}
+
+func (m Form) Init() tea.Cmd {
+	return nil
+}
+
+func (m Form) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "enter":
+			if m.title.Focused() {
+				m.title.Blur()
+				m.description.Focus()
+				return m, textarea.Blink
+			} else {
+				models[form] = m
+				return models[model], m.CreateTask
+			}
+		}
+	}
+	if m.title.Focused() {
+		m.title, cmd = m.title.Update(msg)
+		return m, cmd
+	} else {
+		m.description, cmd = m.description.Update(msg)
+		return m, cmd
+	}
+}
+
+func (m Form) View() string {
+	return lipgloss.JoinVertical(lipgloss.Left, m.title.View(), m.description.View())
+}
+
 func main() {
-	models := []tea.Model{New(), NewForm(todo)}
+	models = []tea.Model{New(), NewForm(todo)}
 	m := models[model]
 	p := tea.NewProgram(m)
 	if _, err := p.Run(); err != nil {
-		fmt.Println("Error running program:", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
